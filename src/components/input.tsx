f5bb7231b28cb83d714ba00cb7b669af3525203b
Jessica Bandeira <jessica.santosb@outlook.com>
2024-10-25 10:51:59 -0300

    feat: handle input search

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,5 +1,32 @@
-import { SearchInput } from './ui/input';
+'use client';
+
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
+import { SearchInput } from '@/components/ui/input';
 
 export function Input() {
-  return <SearchInput type='search' placeholder='search by name' />;
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const params = new URLSearchParams(searchParams);
+    const query = e.currentTarget.value;
+
+    if (query) {
+      params.set('q', query);
+    } else {
+      params.delete('q');
+    }
+
+    replace(`${pathname}?${params.toString()}`);
+  };
+
+  return (
+    <SearchInput
+      type='search'
+      placeholder='search by name'
+      onChange={handleChange}
+    />
+  );
 }
