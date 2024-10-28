d179e77e7d4e594879a3b43e9b6ae332aa2deeba
Jessica Bandeira <jessica.santosb@outlook.com>
2024-10-28 19:39:26 -0300

    style: change main title layout and main background color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { SearchParams } from '@/lib/types';
 
 export default function Home({ searchParams }: SearchParams) {
   return (
-    <main className='p-4 md:p-8'>
+    <main className='bg-primary p-4 md:p-8'>
       <Orders searchParams={searchParams} />
     </main>
   );
