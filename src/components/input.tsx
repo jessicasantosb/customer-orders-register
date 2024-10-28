87531175ffc85711f6dc6069865406ca1de96d1f
Jessica Bandeira <jessica.santosb@outlook.com>
2024-10-28 15:50:28 -0300

    feat: add use-debounce to search input

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -3,24 +3,28 @@
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
 import { SearchInput } from '@/components/ui/input';
+import { useDebouncedCallback } from 'use-debounce';
 
 export function Input() {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const params = new URLSearchParams(searchParams);
-    const query = e.currentTarget.value;
+  const handleChange = useDebouncedCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const params = new URLSearchParams(searchParams);
+      const query = e.target.value;
 
-    if (query) {
-      params.set('q', query);
-    } else {
-      params.delete('q');
-    }
+      if (query) {
+        params.set('q', query);
+      } else {
+        params.delete('q');
+      }
 
-    replace(`${pathname}?${params.toString()}`);
-  };
+      replace(`${pathname}?${params.toString()}`);
+    },
+    500,
+  );
 
   return (
     <SearchInput
